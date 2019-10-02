dd06f03188ede2a98eafd0148e8b7661afda78ec
EugeneBykovsky <bykovsky.ssau@gmail.com>
2019-10-02 14:16:31 +0400

    feat: add task

--- src/index.js
+++ src/index.js
@@ -38,7 +38,39 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    let message = expr;
+    message = message.split('');
+
+    let text = [];
+    let letter = [];
+    let x = 0;
+    let y = 0;
+
+    message.forEach(function (item) {
+    x += 1;
+    letter.push(item);
+        if (x == 10) {
+            letter = letter.join('');
+            text[y] = letter;
+            y += 1;
+            x = 0;
+            letter = [];
+        }
+    });
+
+    text.forEach(function (item, i) {
+    text[i] = item.replace(/10/gi, '.').replace(/11/gi, '-').replace(/0/gi, '').replace('**********', ' ');
+    });
+
+    text.forEach(function (item, i) {
+    if (item in MORSE_TABLE) {
+        text[i] = MORSE_TABLE[item];
+    }
+    });
+
+    let result = text.join('');
+
+    return result;
 }
 
 module.exports = {
